010022e491e5f0bd109819ce742e8df53af198fe
Natalia Mateus <naliamate@gmail.com>
2018-10-05 16:41:54 -0500

    codigos modificados

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -1,11 +1,16 @@
 class TareasController < ApplicationController
+  
+  before_action :set_tarea, except: [:index, :new, :create]
+  
   def index
   	@tareas = Tarea.all
   	#select * from tareas
   end
 
   def new
-  	@tarea = Tarea.new
+  	@tarea = Tarea.new (tarea_params)
+    if @tarea.save
+
   end
 
   	def create
@@ -18,17 +23,36 @@ class TareasController < ApplicationController
 		  end
 	end
   def show
-  	@tarea = Tarea.find(params[:id])
+  	#@tarea = Tarea.find(params[:id])
   	#select * from tareas where id=:id
   end
 
   def destroy
-    @tarea = Tarea.find(params[:id])
+    #@tarea = Tarea.find(params[:id])
     @tarea.destroy
-    redirect_to controller: "tareas", action: "index"
+    redirect_to tareas_path
+    #redirect_to controller: "tareas", action: "index"
   end
 
   def edit
-    @tarea = Tarea.find(params[:id])
+    #@tarea = Tarea.find(params[:id])
+  end
+
+  def update
+  #@tarea = Tarea.find {params|:id|}
+  @tarea.update(titulo: params[:titulo], descripcion: params[:descripcion])
+  redirect_to controller: "tareas", action: 'show', id: @tarea.id
+else
+  render :edit
+  end
+end
+
+private
+  def set_tarea
+    @tarea = Tarea.find {params|:id|}
+  end
+
+  def tarea_params
+    params.require(:tarea).permit(:titulo,:descripcion)
   end
  end
